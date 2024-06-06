714abbedcac08aac1485450db9dc635493ec37d7
dungvinh <dungdv271106@gmail.com>
2024-06-06 20:56:32 +0700

    biendovip

---
+++ gioithieu.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const links = document.querySelectorAll("nav ul li a");
+
+//   links.forEach((link) => {
+//     link.addEventListener("click", function (event) {
+//       event.preventDefault();
+//       const section = document.querySelector(this.getAttribute("href"));
+//       // window.scrollTo({
+//       //   behavior: "smooth",
+//       // });
+//     });
+//   });
+// });
